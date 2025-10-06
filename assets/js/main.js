8a3a87017fd89193170fea3b242d3eaf60fd2976
Tyree Daniels <danieltyree866@gmail.com>
2025-10-05 23:04:51 -0400

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,6 @@
 
 (function () {
   'use strict';
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach((navmenu) => {
-    navmenu.addEventListener('click', function (e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
-  });
 
   /**
    * Preloader
