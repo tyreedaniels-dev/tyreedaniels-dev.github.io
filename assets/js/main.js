94ef482a41b97a91d4e50ed135fb183a4c8b544e
Tyree Daniels <danieltyree866@gmail.com>
2025-10-07 22:36:56 -0400

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,13 +31,6 @@
         : scrollTop.classList.remove('active');
     }
   }
-  scrollTop.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  });
 
   window.addEventListener('load', toggleScrollTop);
   document.addEventListener('scroll', toggleScrollTop);
@@ -54,6 +47,34 @@
     });
   }
   window.addEventListener('load', aosInit);
+
+  // smooth scroll
+  $(document).ready(function () {
+    // Add smooth scrolling to all links
+    $('a').on('click', function (event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== '') {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate(
+          {
+            scrollTop: $(hash).offset().top,
+          },
+          800,
+          function () {
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          }
+        );
+      } // End if
+    });
+  });
 
   /**
    * Init typed.js
