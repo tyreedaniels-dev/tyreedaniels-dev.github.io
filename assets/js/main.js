28089beab3978bb8536a3a562b927af06bebdb15
Tyree Daniels <danieltyree866@gmail.com>
2025-11-13 14:44:27 -0500

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,4 +182,17 @@
   }
 
   window.addEventListener('load', initSwiper);
+
+  let count_particles, update;
+  count_particles = document.querySelector('.js-count-particles');
+  update = function () {
+    if (
+      window.pJSDom[0].pJS.particles &&
+      window.pJSDom[0].pJS.particles.array
+    ) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+  };
+  requestAnimationFrame(update);
 })();
